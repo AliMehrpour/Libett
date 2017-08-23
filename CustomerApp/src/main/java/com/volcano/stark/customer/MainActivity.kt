c55b8fc516987874cdbd07d780693a6ca0011d6b
Ali Mehrpour <alimehrpour@gmail.com>
2017-08-22 22:18:06 -0700

    Create the project

---
+++ CustomerApp/src/main/java/com/volcano/stark/customer/MainActivity.kt
@@ -0,0 +1,13 @@
+// Copyright (c) 2017 Volcano Inc. All rights reserved.
+package com.volcano.stark.customer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
